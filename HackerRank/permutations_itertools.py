335cdb63fbdbe0e2f012da3c000ffc168eb510d7
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2021-06-17 15:22:26 +0500

    Update permutations_itertools.py

--- HackerRank/permutations_itertools.py
+++ HackerRank/permutations_itertools.py
@@ -1,4 +1,4 @@
-# In this problem, the programmer is supposed to solve the problem using permutations functions of the itertools library
+# In this problem, the programmer is supposed to solve the problem using permutations function of the itertools library
 # The difficulty of the problem is EASY
 # Link to the problem: https://www.hackerrank.com/challenges/itertools-permutations/problem
 # All test cases passed
