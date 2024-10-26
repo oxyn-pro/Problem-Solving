5fc57787b50be59e29f43bdf5fd574a04e3c0a0e
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-10-26 18:12:47 +0500

    Add files via upload

---
+++ LeetCode/27_remove_element.py
@@ -0,0 +1,14 @@
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+val = 2
+
+
+def remove_element(nums, val):
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] != val:
+            nums[left] = nums[right]
+            left += 1
+    return left
+
+
+remove_element(nums, val)
