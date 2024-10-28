48ad6797a9d785e04525fc4baf3bbeda78441c92
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-10-28 16:09:34 +0500

    Add files via upload

---
+++ LeetCode/35_search_insert_position.py
@@ -0,0 +1,23 @@
+nums = [1, 3, 5, 7]
+target = 6
+
+
+def search_insert(nums, target):
+    h, l = len(nums) - 1, 0
+
+    while l <= h:
+        mid = (h + l) // 2
+        mid_val = nums[mid]
+
+        if mid_val == target:
+            return mid
+
+        if mid_val > target:
+            h = mid - 1
+        elif mid_val < target:
+            l = mid + 1
+
+    return l
+
+
+search_insert(nums, target)
