65db3c161cc145959b73ac3a2fe2ab3192a656e3
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-10-28 18:31:48 +0500

    Add files via upload

---
+++ LeetCode/682_baseball_game.py
@@ -0,0 +1,19 @@
+operations = ["5", "-2", "4", "C", "D", "9", "+", "+"]
+
+
+def cal_points(operations):
+    st = []
+    for op in operations:
+        if op == "C":
+            st.pop()
+        elif op == "D":
+            st.append(st[-1] * 2)
+        elif op == "+":
+            st.append(st[-1] + st[-2])
+        else:
+            st.append(int(op))
+
+    return sum(st)
+
+
+cal_points(operations)
