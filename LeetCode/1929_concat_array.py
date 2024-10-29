5dcbdf5da842bd60c284159949a985dd7d20bb3f
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-10-29 18:12:34 +0500

    Update 1929_concat_array.py

--- LeetCode/1929_concat_array.py
+++ LeetCode/1929_concat_array.py
@@ -1,17 +1,21 @@
-nums = [1, 2, 1]
-
 # There are definitely more solutions to this problem but I have only included 3 of them.
 # The third solution concat3 is food for thought...
 
 
+# TC: O(n)
+# SC: O(n)
 def concat1(nums):
     return nums + nums
 
 
+# TC: O(n)
+# SC: O(n)
 def concat2(nums):
     return nums * 2
 
 
+# TC: O(n)
+# SC: O(n)
 def concat3(nums):
     ans = []
     for i in range(2):
@@ -20,4 +24,5 @@ def concat3(nums):
     return ans
 
 
+nums = [1, 2, 1]
 concat3(nums)
