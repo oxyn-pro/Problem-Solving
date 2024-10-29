2ce20b33c6a37bc4c95e827741ef8493aaf342e0
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-10-29 18:13:16 +0500

    Update 27_remove_element.py

--- LeetCode/27_remove_element.py
+++ LeetCode/27_remove_element.py
@@ -1,7 +1,5 @@
-nums = [0, 1, 2, 2, 3, 0, 4, 2]
-val = 2
-
-
+# TC: O(n)
+# SC: O(1)
 def remove_element(nums, val):
     left = 0
     for right in range(len(nums)):
@@ -11,4 +9,6 @@ def remove_element(nums, val):
     return left
 
 
+nums = [0, 1, 2, 2, 3, 0, 4, 2]
+val = 2
 remove_element(nums, val)
