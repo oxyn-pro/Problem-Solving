ff7c89a34dbedaeb674d9f741b351d688b204a0e
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2024-12-27 17:59:48 +0500

    Update 200_number_islands.py

--- LeetCode/200_number_islands.py
+++ LeetCode/200_number_islands.py
@@ -1,39 +1,77 @@
 from collections import deque
 
 
-# TC: O(n^2)
-# SC: O(n^2)
-def shortestPathBinaryMatrix(grid) -> int:
+# TC: O(n * m)
+# SC: O(n * m)
+def numIslands(grid):
+    """I implemented it with allocating space for the "visit" hashset"""
+    ROWS, COLS = len(grid), len(grid[0])
+    visit = set()
+
+    def dfs(r, c, visit):
+        if (
+            min(r, c) < 0
+            or r == ROWS
+            or c == COLS
+            or (r, c) in visit
+            or grid[r][c] == "0"
+        ):
+            return
+
+        visit.add((r, c))
+
+        dfs(r - 1, c, visit)
+        dfs(r + 1, c, visit)
+        dfs(r, c - 1, visit)
+        dfs(r, c + 1, visit)
+
+    count = 0
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == "1" and not (r, c) in visit:
+                count += 1
+                dfs(r, c, visit)
+
+    return count
+
+
+def numIslands_bfs(grid):
     ROWS, COLS = len(grid), len(grid[0])
     queue = deque()
-    visit = set()
-    neighbors = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [1, -1], [-1, 1], [1, 1]]
 
-    queue.append((0, 0))
-    visit.add((0, 0))
-    length = 1
+    neighbors = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
-    if grid[0][0] == 1:
-        return -1
-
-    while queue:
-        for _ in range(len(queue)):
+    def bfs():
+        while queue:
             r, c = queue.popleft()
 
-            if r == ROWS - 1 and c == COLS - 1:
-                return length
+            if min(r, c) < 0 or r == ROWS or c == COLS or grid[r][c] == "0":
+                continue
+
+            grid[r][c] = "0"
 
             for dr, dc in neighbors:
-                if (
-                    min(r + dr, c + dc) < 0
-                    or r + dr == ROWS
-                    or c + dc == COLS
-                    or (r + dr, c + dc) in visit
-                    or grid[r + dr][c + dc] == 1
-                ):
-                    continue
-                queue.append((r + dr, c + dc))
-                visit.add((r + dr, c + dc))
-        length += 1
+                if (r + dr, c + dc) != "0":
+                    queue.append((r + dr, c + dc))
 
-    return -1
+    count = 0
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == "1":
+                count += 1
+                queue.append((r, c))
+                bfs()
+
+    return count
+
+
+grid = [
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"],
+]
+numIslands(grid)
+
+
+numIslands_bfs(grid)
