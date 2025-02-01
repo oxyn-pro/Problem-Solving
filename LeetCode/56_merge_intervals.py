906b8a432f13888b41560e94a48da0486da799ef
Okhunjon Kuvatov <69118015+oxyn-pro@users.noreply.github.com>
2025-02-01 21:02:04 +0500

    Add files via upload

---
+++ LeetCode/56_merge_intervals.py
@@ -0,0 +1,19 @@
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+
+
+# TC: O(n log n)
+# SC: O(n)
+def merge(intervals):
+    intervals.sort(key=lambda x: x[0])
+    res = [intervals[0]]
+
+    for i in range(1, len(intervals)):
+        if intervals[i][0] <= res[-1][1]:
+            res[-1][1] = max(res[-1][1], intervals[i][1])
+        else:
+            res.append(intervals[i])
+
+    return res
+
+
+merge(intervals)
